404d701edbd5a558588635fd9636025ee02b94fb
TheKroyli <bkorob47@outlook.com>
2019-10-19 15:58:32 +0300

    TextOutput

---
+++ Starter/L1/TextOutputApp/TextOutputApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace TextOutputApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Text_Box1.Text = Text_Box2.Text; Text_Box1.Text = Text_Box2.Text;
+        }
+    }
+}
