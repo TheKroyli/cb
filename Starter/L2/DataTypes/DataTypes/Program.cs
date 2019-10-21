cfe23132bcb880eb1cf46ad0c991645478213462
TheKroyli <bkorob47@outlook.com>
2019-10-21 16:55:25 +0300

    Data Types App

--- Starter/L2/DataTypes/DataTypes/Program.cs
+++ Starter/L2/DataTypes/DataTypes/Program.cs
@@ -5,7 +5,24 @@ namespace DataTypes
     class Program
     {
         static void Main(string[] args)
+
         {
+            double pi = 3.141592653;
+            decimal e;
+            e = 2.7182818284590452M;
+
+            string a = "\nмоя строка 1";
+            string b = "\tмоя строка 2";
+            string c = "\aмоя строка 3";
+
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Starter/L2/DataTypes/DataTypesForm/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+
+namespace DataTypesForm
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("От 0 до 255");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("От -128 до 127");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("От -32 768 до 32 767");
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("От 0 до 65 535");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("От -2 147 483 648 до 2 147 483 647");
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("От 0 до 4 294 967 295");
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("От -9 223 372 036 854 775 808 до 9 223 372 036 854 775 807");
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("От 0 до 18 446 744 073 709 551 615");
+        }
+    }
+}
