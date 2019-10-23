31b1146745c6009e543decd8157f2013b3981671
TheKroyli <bkorob47@outlook.com>
2019-10-23 20:45:00 +0300

    AreaOfACircle

--- Starter/L3/AreaOfACircle/AreaOfACircle/Program.cs
+++ Starter/L3/AreaOfACircle/AreaOfACircle/Program.cs
@@ -6,6 +6,17 @@ namespace AreaOfACircle
     {
         static void Main(string[] args)
         {
+            const double pi = 3.141;
+            double r = 6;
+            int stepen = 2;
+            double result;
+
+            result = pi * Math.Pow(6, stepen);
+
+            Console.Write("Радиус круга равен : ");
+            Console.WriteLine(result);
+            Console.ReadKey();
+
         }
     }
 }
