15d5ee5a7c735a7c6e8d780233775b41928f583b
TheKroyli <bkorob47@outlook.com>
2019-10-23 21:26:06 +0300

    Volume

--- Starter/L3/Volume/Volume/Program.cs
+++ Starter/L3/Volume/Volume/Program.cs
@@ -6,6 +6,24 @@ namespace Volume
     {
         static void Main(string[] args)
         {
+            const double pi = 3.141;
+            double V = 3, S = 14, R = 12, h = 8, _volume, area;
+            double stepen = 2;
+
+
+            _volume = pi * Math.Pow(R, stepen) * h;
+            area = (2 * pi * Math.Pow(R, stepen) * h) + (2 * pi * Math.Pow(R, stepen) * h);
+            area = 2 * pi * R * ((R + h));
+
+            Console.Write("Объем равен : ");
+            Console.WriteLine(_volume);
+
+            Console.Write("Площадь  равен : ");
+            Console.WriteLine(area);
+
+            Console.ReadKey();
+
+
         }
     }
 }
