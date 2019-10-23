7499d1b844e80aa1e9768b15e05e70681b980036
TheKroyli <bkorob47@outlook.com>
2019-10-23 19:32:39 +0300

    OperationsForVariables

--- Starter/L3/OperationsForVariables/OperationsForVariables/Program.cs
+++ Starter/L3/OperationsForVariables/OperationsForVariables/Program.cs
@@ -6,6 +6,33 @@ namespace OperationsForVariables
     {
         static void Main(string[] args)
         {
+            int x = 10, y = 12, z = 3;
+            {
+                x += (y - (x++) * z);
+                Console.WriteLine(x);
+            }
+
+            {
+                z = --x - y * 5;
+                Console.WriteLine(z);
+            }
+
+            {
+                y /= x + 5 % z;
+                Console.WriteLine(y);
+            }
+
+            {
+                z = x++ + y * 5;
+                Console.WriteLine(z);
+            }
+
+            {
+                x = y - x++ * z;
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
         }
     }
 }
