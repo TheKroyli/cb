fff9e13c299b02b79366c3b5972afaf90aab3c98
TheKroyli <bkorob47@outlook.com>
2019-10-23 19:55:00 +0300

    Average

--- Starter/L3/Average/Average/Program.cs
+++ Starter/L3/Average/Average/Program.cs
@@ -6,6 +6,17 @@ namespace Average
     {
         static void Main(string[] args)
         {
+            var a = 5;
+            var b = 13;
+            var c = 51;
+            int result;
+
+            result = ((a + b + c) / 3);
+
+            Console.Write("Результат : ");
+            Console.WriteLine(result);
+            Console.ReadKey();
+
         }
     }
 }
