0ee8e0651015e28e1b72b54acffb29c9e0f7c84f
TheKroyli <bkorob47@outlook.com>
2019-10-31 18:33:19 +0300

    L4Fo0To100

--- Starter/L4/L4Fo0To100/L4Fo0To100/Program.cs
+++ Starter/L4/L4Fo0To100/L4Fo0To100/Program.cs
@@ -6,6 +6,34 @@ namespace L4Fo0To100
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число :");
+
+            string number1 = Console.ReadLine();
+
+            int Number = Convert.ToInt32(number1);
+
+            if (Number >= 0 && Number <= 14)
+            {
+                Console.WriteLine("Вы ввели {0}, числовой промежуток от 0 до 14", Number);
+            }
+            else if (Number >= 15 && Number <= 35)
+            {
+                Console.WriteLine("Вы ввели {0}, числовой промежуток от 15 до 35", Number);
+            }
+            else if (Number >= 36 && Number <= 50)
+            {
+                Console.WriteLine("Вы ввели {0}, числовой промежуток от 36 до 50", Number);
+            }
+            else if (Number >= 50 && Number <= 100)
+            {
+                Console.WriteLine("Вы ввели {0}, числовой промежуток от 50 до 100", Number);
+            }
+            else
+            {
+                Console.WriteLine("Данное число не входит не в один числовой промежуток");
+            }
+
+            Console.ReadKey();
         }
     }
 }
