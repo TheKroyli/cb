4b536d160afc9a3dc447783aadb6afda0e8e69a9
TheKroyli <bkorob47@outlook.com>
2019-10-31 18:46:34 +0300

    L4Сalculator

--- Starter/L4/L4Сalculator/L4Сalculator/Program.cs
+++ Starter/L4/L4Сalculator/L4Сalculator/Program.cs
@@ -6,6 +6,55 @@ namespace L4Сalculator
     {
         static void Main(string[] args)
         {
+
+            int op1 = 5;
+            int op2 = 0;
+
+            Console.WriteLine("Первое число = {0}", op1);
+            Console.WriteLine("Второе число = {0}", op2);
+            Console.WriteLine("Введите знак арифметической операции : ");
+
+            string sigh = Console.ReadLine();
+
+            int decision;
+
+            Console.ReadKey();
+            switch (sigh)
+            {
+                case "+":
+                    decision = op1 + op2;
+                    Console.Write(decision);
+                    break;
+
+                case "-":
+                    decision = op1 - op2;
+                    Console.Write(decision);
+                    break;
+
+                case "*":
+                    decision = op1 * op2;
+                    Console.Write(decision);
+                    break;
+
+                case "/":
+                    if (op1 == 0)
+                    {
+                        Console.WriteLine("Первое число = 0, на 0 делить нельзя");
+                        Console.ReadKey();
+                        return;
+                    }
+                    else if (op2 == 0)
+                    {
+                        Console.WriteLine("Второе число = 0, на 0 делить нельзя");
+                        Console.ReadKey();
+                        return;
+                    }
+                    decision = op1 / op2;
+                    Console.Write(decision);
+                    break;
+            }
+
+            Console.ReadKey();
         }
     }
 }
