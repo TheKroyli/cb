ce7b626f5dc3fb17fd560b6b37523f755e501b13
TheKroyli <bkorob47@outlook.com>
2019-11-08 19:52:02 +0300

    L5Stepen2

--- Starter/L5/L5Stepen2/L5Stepen2/Program.cs
+++ Starter/L5/L5Stepen2/L5Stepen2/Program.cs
@@ -6,6 +6,24 @@ namespace L5Stepen2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число -> ");
+
+            string value = Console.ReadLine();
+            int number = Convert.ToInt32(value);
+
+            int result;
+            result = number - 1;
+
+            if ((number & result) == 0)
+            {
+                Console.WriteLine("Число является степенью двойки");
+            }
+            else
+            {
+                Console.WriteLine("Число НЕ является степенью двойки");
+            }
+
+            Console.ReadKey();
         }
     }
 }
