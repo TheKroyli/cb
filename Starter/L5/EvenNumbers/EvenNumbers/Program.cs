b2b14ffb94e881c00d749917f0d2024838ebc045
TheKroyli <bkorob47@outlook.com>
2019-11-09 21:54:55 +0300

    L5EvenNumbers

--- Starter/L5/EvenNumbers/EvenNumbers/Program.cs
+++ Starter/L5/EvenNumbers/EvenNumbers/Program.cs
@@ -6,6 +6,43 @@ namespace EvenNumbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Метод №1");
+            Console.WriteLine("Введите число");
+            string M1number1 = Console.ReadLine();
+            int M1number = int.Parse(M1number1);
+
+            int result = M1number & 1;
+
+            if (result == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+
+            else
+            {
+                Console.WriteLine("Число НЕчетное");
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Метод №2");
+            Console.WriteLine("Введите число");
+            string M2number1 = Console.ReadLine();
+            int M2number = int.Parse(M2number1);
+
+            if (M2number % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+
+            else
+            {
+                Console.WriteLine("Число НЕчетное");
+            }
+
+            Console.ReadKey();
         }
     }
 }
