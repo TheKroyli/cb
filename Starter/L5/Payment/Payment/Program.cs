c86264f13c536658a9ed953cce417076189663b3
TheKroyli <bkorob47@outlook.com>
2019-11-09 21:50:34 +0300

    L5Payment

--- Starter/L5/Payment/Payment/Program.cs
+++ Starter/L5/Payment/Payment/Program.cs
@@ -6,6 +6,28 @@ namespace Payment
     {
         static void Main(string[] args)
         {
+            int x = 5, y = 10, z = 15;
+
+            x += y >> x++ * z;
+            Console.WriteLine("Выражение 'x += y >> x++ * z' имеет такой результат x={0} y={1} z={2}", x, y, z);
+            Console.WriteLine("");
+
+            z = ++x & y * 5;
+            Console.WriteLine("Выражение 'z = ++x & y * 5' имеет такой результат x={0} y={1} z={2}", x, y, z);
+            Console.WriteLine("");
+
+            y /= x + 5 | z;
+            Console.WriteLine("Выражение 'y /= x + 5 | z' имеет такой результат x={0} y={1} z={2}", x, y, z);
+            Console.WriteLine("");
+
+            z = x++ & y * 5;
+            Console.WriteLine("Выражение 'z = x++ & y * 5' имеет такой результат x={0} y={1} z={2}", x, y, z);
+            Console.WriteLine("");
+
+            x = y << x++ ^ z;
+            Console.WriteLine("Выражение 'x = y << x++ ^ z' имеет такой результат x={0} y={1} z={2}", x, y, z);
+
+            Console.ReadKey();
         }
     }
 }
