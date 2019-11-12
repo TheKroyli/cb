14b30a7f500d6e34d7f47413904cea6f153afa28
TheKroyli <bkorob47@outlook.com>
2019-11-12 14:01:40 +0300

    Cleaned the using in the L6Delivery project

---
+++ Starter/L6/Delivery/Delivery/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Starter/L6/Delivery/Delivery/ReadMe.txt
@@ -0,0 +1,2 @@
+﻿+ Используя Visual Studio, создал проект по шаблону Console Application.
+   + Для работы использован цикл do-while.
