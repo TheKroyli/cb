92484b383cec04184beae3c86a8f0d2bb552d5af
TheKroyli <bkorob47@outlook.com>
2019-11-12 14:04:25 +0300

    L6Delivery

--- Starter/L6/Delivery/Delivery/Program.cs
+++ Starter/L6/Delivery/Delivery/Program.cs
@@ -6,6 +6,21 @@ namespace Delivery
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число клиентов");
+            string customersS = Console.ReadLine();
+            int customers = Int32.Parse(customersS);
+
+            int factorial = 1;
+
+            do
+            { 
+                factorial *= customers--;
+            }
+            while (customers > 0);
+
+            Console.WriteLine("Для {0} клиентов имеется {1} маршрута", customersS, factorial);
+
+            Console.ReadKey();
         }
     }
 }
